b50ec9331d5286882da149afa3e668d05bc62280
Brayhan De Aza <brayhandeaza@Brayhans-MacBook-Pro.local>
2020-08-20 18:34:07 -0400

    canvas size fixed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './css/App.scss'
 import { Switch, Route } from 'react-router-dom';
 
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 
 // Components
-import Home from './views/Home'
-import Aside from './components/Aside'
-import Menu from './components/Menu'
+import Home from "./Views/Home"
+// import Aside from './components/Aside'
+// import Menu from './components/Menu'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      
-    }
-  }
-  render(){
-    return (
-      <Switch>
-          <Route exact  path='/' component={Home}/>
-          <Route exact  path='/charts' component={Home}/>
-          <Route exact  path='/news' component={Home}/>
-          <Route exact  path='/calculator' component={Home}/>
-      </Switch>
-    )
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+
+		}
+	}
+	render() {
+		return (
+			<Switch>
+				<Route exact path='/' component={Home} />
+			</Switch>
+		)
+	}
 }
 function mapStateToProps(state) {
-  return {state}
+	return { state }
 }
 export default connect(mapStateToProps)(App)
